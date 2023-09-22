ce8ffbeebf498f53ba66359d298fc4210e29540b
Omewah <joelomewah@ymail.com>
2023-09-22 11:39:46 -0700

    creates a file with puppet

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,10 @@
 # creates a file in /tmp
 
-file { '/tmp/school':
-ensure => 'file',
+file { 'school':
+ensure => 'present',
+path => '/tmp/school',
 mode => '0744',
 owner => 'www-data',
 group => 'www-data',
-content => 'I love Puppet',
+content => 'I love Puppet'
 }
