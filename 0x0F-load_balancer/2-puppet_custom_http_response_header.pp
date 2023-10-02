be36883f0901e2e0d514f6773d4c51240bf6aa87
Omewah <joelomewah@ymail.com>
2023-10-02 12:55:37 -0700

    create customer HTTP header with puppet

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+# create a custom HTTP header with pupppet
+exec { 'http header':
+	command  => 'sudo apt-get update -y;
+	sudo apt-get install nginx -y;
+	sudo sed -i "/server_name _/a add_header X-Served-By $HOSTNAME;" /etc/nginx/sites-available/default
+	sudo service nginx restart',
+	provider => shell,
+}
